6368408ce4d7af5732dbc4f4730ac1b6a8fb42ed
sufyan20033 <sufyanlehri10@gmail.com>
2025-04-30 01:17:01 +0500

    Update authroute.js

--- Web-App/Web-Backend/routes/authroute.js
+++ Web-App/Web-Backend/routes/authroute.js
@@ -2,12 +2,6 @@ import express from "express";
 import { verifyToken } from "../controllers/verifyTokens.js";
 const router = express.Router();
 
-
-
-
 router.post('/verifyToken', verifyToken);
-
-
-
 
 export default router;
